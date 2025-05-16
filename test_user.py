00c4b7282beddd008e4d1edbac5a8503361c7c0d
stephniea <stephnieasam@gmail.com>
2025-05-16 00:24:14 -0400

    Update test_user.py

--- test_user.py
+++ test_user.py
@@ -1,13 +1,20 @@
 import unittest
 from user import User
 
+# unit test for the user class in relation to course management and avaliability updates
 class TestUser(unittest.TestCase):
-#This sees if user would be allowed to add course
+# tests the add_course method of the user class
+# creates a user object with test data to ensure that the method correctly appends the new course to the list
+# makes sure that there's nothing preventing the addition of another course
+# assert function to ensure that the  new course was added to the user.courses
     def test_add_course(self):
         user= User("Alice", "Computer Science", ["CS101"], "Afternoons", "MWF 2-4pm")
         user.add_course("Math101")
         self.assertIn("Math101", user.courses)
-#Shows and checks to see if the users availability is correctly up to date
+# tests the update_avaliability method of the user class
+# makes sure a users avaliability is properly updated when a change is made to the time
+# creates a user object with test data and replaces the old avaliability with the new one
+# asserts function used to confirm that the avalibility was changed 
     def test_update_availability(self):
         user = User("Alice", "Computer Science", ["CS101"], "Afternoons", "MWF 2-4pm")
         user.update_availability("Evenings")
